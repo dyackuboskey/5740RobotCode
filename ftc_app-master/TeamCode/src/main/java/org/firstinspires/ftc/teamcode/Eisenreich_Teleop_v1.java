a801399df94a0d7d9336bbf09e9b4b0b9613caa8
Colton Eiserneich <ceisenreich99@hotmail.com>
2017-11-29 13:23:52 -0500

    Eisenreich_Autov2

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Eisenreich_Teleop_v1.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Eisenreich_Teleop_v1.java
@@ -1,70 +1,75 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by Colton on 11/7/2017.
+ * Created by Colton on 11/21/2017.
  */
 
-@Autonomous
-public class Colton_RedAutov1 extends LinearOpMode {
+public class Eisenreich_Teleop_v1 extends LinearOpMode {
+
     private DcMotor FrontL;
     private DcMotor FrontR;
     private DcMotor BackL;
     private DcMotor BackR;
-    private ColorSensor color;
-    private Servo colorArm;
+    private Servo lGrab;
+    private Servo rGrab;
+    private DcMotor Spool;
 
     @Override
     public void runOpMode() {
+
         FrontL = hardwareMap.dcMotor.get("FrontL");
         FrontR = hardwareMap.dcMotor.get("FrontR");
         BackL = hardwareMap.dcMotor.get("BackL");
         BackR = hardwareMap.dcMotor.get("BackR");
-
-        colorArm = hardwareMap.servo.get("BillyJean");
-
-        color = hardwareMap.colorSensor.get("Billy");
-
-        RobotDrive drive = new RobotDrive(FrontL, FrontR, BackL, BackR);
 
         FrontL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        Spool = hardwareMap.dcMotor.get("Spool");
+        Spool.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        RobotDrive drive = new RobotDrive(FrontL, FrontR, BackL, BackR);
+
+        lGrab = hardwareMap.servo.get("LeftGrab");
+        rGrab = hardwareMap.servo.get("RightGrab");
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        colorArm.setPosition(0);
-
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
+
+        double LStickY = gamepad1.left_stick_y;
+        double LStickX = gamepad1.left_stick_x;
+        double RStickX = gamepad1.right_stick_x;
+        double Grab = gamepad1.right_trigger;
+        double spool = gamepad1.left_trigger;
+
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             telemetry.addData("Status", "Running");
             telemetry.update();
 
-            int colorB = color.blue();
-            colorArm.setPosition(.5);
+            drive.mechanumDrive(LStickY, LStickX, RStickX);
 
-            if(colorB > 700){
-                drive.mechanumDrive(0, .5,0);
-                sleep(1000);
-                drive.mechanumDrive(0, 0, 0);
-                colorArm.setPosition(0);
+            Spool.setPower(spool);
+            
+            if(Grab > 1){
+                lGrab.setPosition(1);
+                rGrab.setPosition(0);
             }
             else{
-                drive.mechanumDrive(0, -.5,0);
-                sleep(1000);
-                drive.mechanumDrive(0, 0, 0);
-                colorArm.setPosition(0);
+                lGrab.setPosition(.5);
+                rGrab.setPosition(.5);
             }
+
 
         }
     }
